7edc715798ebaf7eb4cc3ffb257daa1212383ca1
ALewis <ALewis@spartaglobal.com>
2023-04-13 13:57:53 +0100

    added small loop do diplay breakpoint in debud mode

--- HelloWorld/HelloWorldApp/Program.cs
+++ HelloWorld/HelloWorldApp/Program.cs
@@ -15,6 +15,15 @@ namespace HelloWorld
             // variables/  in camelCase
             // class/ methods/ properties in PascalCase
             //
+
+            int x = 100;
+            x += 10;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("i: " + i + "x " + x);
+                x += i;
+            }
+
         }
     }
 }
