6c9971050cb75bae170c0d15954b37ff8caf675d
ALewis <ALewis@spartaglobal.com>
2023-04-13 14:48:21 +0100

    created unit test environment and setup test method

---
+++ UnitTestLesson/CodeToTest/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeToTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOfDay = 21;
+            NewMethod(timeOfDay);
+        }
+
+        private static void NewMethod(int timeOfDay)
+        {
+            if (timeOfDay >= 5 && timeOfDay < 12)
+            {
+                Console.WriteLine("Good Morning");
+            }
+            else if (timeOfDay >= 12 && timeOfDay < 18)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            else
+            {
+                Console.WriteLine("Good Evening");
+            }
+        }
+
+    }
+}
