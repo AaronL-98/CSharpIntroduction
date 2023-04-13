73444ccb9c09565d883f86732aa9111ddef7c1aa
ALewis <ALewis@spartaglobal.com>
2023-04-13 12:12:04 +0100

    changed file headed from preset template to include sytem file headers

--- HelloWorld/HelloWorldApp/Program.cs
+++ HelloWorld/HelloWorldApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
