3fee1d09386f2f427291a143a34d1244cdb27622
ALewis <ALewis@spartaglobal.com>
2023-04-16 23:26:34 +0100

    updatedto throw range excepts and quivelent test for output

--- OperatorsApp/Methods.cs
+++ OperatorsApp/Methods.cs
@@ -6,15 +6,44 @@ using System.Threading.Tasks;
 
 namespace OperatorsApp
 {
+    public struct StonePound
+    {
+        int stone;
+        int pound;
+    }
+
     public class Methods
     {
         public static int GetStones(int totalPounds)
         {
-            return totalPounds / 14; // return whole number of stones
+            if (totalPounds < 0)
+            {
+                Console.WriteLine("Parameter must Be Positive");
+                throw new ArgumentException("Invalid Parameters: Must be Positive");
+            }
+            else if (totalPounds > 1400)
+            {
+                Console.WriteLine("Parameter Excedes Limit");
+                throw new ArgumentException("Invalid Parameters: Exceed Limit");
+            }
+
+                return totalPounds / 14; // return whole number of stones
         }
 
         public static int GetPounds(int totalPounds)
         {
+            if (totalPounds < 0)
+            {
+                Console.WriteLine("Parameter must Be Positive");
+                throw new ArgumentException("Invalid Parameters: Must be Positive");
+
+            }
+            else if (totalPounds > 1400)
+            {
+                Console.WriteLine("Parameter Excedes Limit");
+                throw new ArgumentException("Invalid Parameters: Exceed Limit");
+            }
+
             return totalPounds % 14; // return number of pounds left over
         }
     }
--- OperatorsControlFlowTests/StoneTests.cs
+++ OperatorsControlFlowTests/StoneTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32.SafeHandles;
-using OperatorsApp;
+﻿using OperatorsApp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,27 +18,46 @@ namespace OperatorsControlFlowTests
         [TestCase(156, 11)] // Pass
         [TestCase(156, 14)] // Fail
         [TestCase("156", 11)] // Pass
-        [TestCase(-156, -11)]
+        [TestCase(-156, -11)] // Fail
         public void GivenTotalPounds_GetStones_ReturnNumberOfStones(int totalPounds, int expected)
         {
             Assert.That(Methods.GetStones(totalPounds), Is.EqualTo(expected));
         }
 
-        [TestCase(1)] // Pass
-        [TestCase(156)] // Fail
-        [TestCase("156")] // Pass
-        public void GivenTotalPounds_GetStones_ReturnNumberOfStones(int totalPounds)
-        {
-            Assert.NotZero(Methods.GetStones(totalPounds));
-        }
-
         [TestCase(156, 2)] // Pass
         [TestCase(156, 4)] // Fail
-        [TestCase((int)156.0, 2)] // Fail
+        [TestCase((int)156.0, 2)] // Pass
         [TestCase("156", 2)] // Pass
+        [TestCase(-156, -11)] // Fail
         public void GivenTotalPounds_GetPounds_ReturnRemainingNumberOfPounds(int totalPounds, int expected)
         {
             Assert.That(Methods.GetPounds(totalPounds), Is.EqualTo(expected));
         }
+
+        [Test]
+        public void GivenTotalPounds_GetStones_ReturnExceptionInvalidNotPositive()
+        {
+            Assert.Throws<ArgumentException>(() => Methods.GetPounds(-1));
+        }
+
+        [Test]
+        public void GivenTotalPounds_GetStones_ReturnExceptionInvalidOverLimit()
+        {
+            Assert.Throws<ArgumentException>(() => Methods.GetPounds(1500));
+        }
+
+        [Test]
+        public void GivenTotalPounds_GetPounds_ReturnExceptionInvalidNotPositive()
+        {
+            Assert.Throws<ArgumentException>(() => Methods.GetPounds(-1));
+        }
+
+        [Test] // Pass
+        public void GivenTotalPounds_GetPounds_ReturnExceptionOverLimit()
+        {
+
+            Assert.Throws<ArgumentException>(() => Methods.GetPounds(1500));
+        }
+
     }
 }
