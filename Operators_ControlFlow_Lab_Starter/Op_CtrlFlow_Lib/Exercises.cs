05a49e681d3731cb5cb51f11096a86c441056833
ALewis <ALewis@spartaglobal.com>
2023-04-17 14:56:29 +0100

    Implemented ReadMe activities

--- Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Op_CtrlFlow
 {
     public class Exercises
     {
+        // Returns if nums are equal or if num1 is divisable by num2
         public static bool MyMethod(int num1, int num2)
         {
             return num1 == num2 ? false : (num1 % num2) == 0;
@@ -12,7 +16,7 @@ namespace Op_CtrlFlow
         // returns the average of the array nums
         public static double Average(List<int> nums)
         {
-            return -1;
+            return nums.Count() == 0 ? 0 : nums.Average();
         }
 
         // returns the type of ticket a customer is eligible for based on their age
@@ -23,19 +27,42 @@ namespace Op_CtrlFlow
         // "Free" if they are under 5
         public static string TicketType(int age)
         {
-            string ticketType = string.Empty;
-            return ticketType;
+            if (age < 5) return "Free";
+            else if (age < 13) return "Child";
+            else if (age < 18) return "Student";
+            else if (age < 60) return "Standard";
+            else return "OAP";
         }
 
         public static string Grade(int mark)
         {
-            var grade = "";
-            return grade;
+            if (mark < 0 && mark > 100) throw new ArgumentOutOfRangeException("Invalid Parameters");
+            else if (mark > 39 && mark < 60) return "Pass";
+            else if (mark > 59 && mark < 75) return "Pass with Merit";
+            else if (mark > 74) return "Pass with Distinction";
+            else return "Fail";
+
+            //  return mark < 40 ? "Fail" : mark < 60 ? "Pass" : mark < 75 ? "Pass with Merit" : "Pass with Distinction";
+
         }
 
         public static int GetScottishMaxWeddingNumbers(int covidLevel)
         {
-            return 0;
+            switch (covidLevel)
+            {
+                case 4:
+                    return 20;
+                case 3:
+                    return 50;
+                case 2:
+                    return 50;
+                case 1:
+                    return 100;
+                case 0:
+                    return 200;
+                default:
+                    throw new ArgumentOutOfRangeException("Invalid Parameters: accepts 0 - 4");
+            }
         }
     }
 }
--- Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
+++ Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
@@ -1,12 +1,27 @@
 ﻿using NUnit.Framework;
 using Op_CtrlFlow;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Op_CtrlFlow_Tests
 {
     public class Exercises_Tests
-    {     
+    {
         // write unit test(s) for MyMethod here
+        [TestCase(10, 10, false)]
+        public void MyMethod_ReturnsFalseIfParametersEqual(int num1, int num2, bool expected)
+        {
+            Console.WriteLine(Exercises.MyMethod(num1, num2));
+            Assert.That(Exercises.MyMethod(num1, num2), Is.EqualTo(expected));
+        }
+        [TestCase(5, 10, false)]
+        [TestCase(10, 5, true)]
+        public void MyMethod_ReturnsIfNum1DevisableByNum2(int num1, int num2, bool expected)
+        {
+            Assert.That(Exercises.MyMethod(num1, num2), Is.EqualTo(expected));
+        }
+
 
 
         [Test]
@@ -37,6 +52,46 @@ namespace Op_CtrlFlow_Tests
         {
             var result = Exercises.TicketType(age);
             Assert.That(result, Is.EqualTo(expected));
+        }
+
+        public class GradeTests
+        {
+            [TestCase(0, "Fail")]                   // Pass
+            [TestCase(40, "Pass")]                  // Pass
+            [TestCase(61, "Pass with Merit")]       // Pass
+            [TestCase(76, "Pass with Distinction")] // Pass
+            public void GivenScore_CheckGrade_ReturnStringContainingGrade(int score, string expected)
+            {
+                try
+                {
+                    Assert.That(Exercises.Grade(score), Is.EqualTo(expected));
+                }
+                catch (Exception e)
+                {
+                    Assert.Throws<ArgumentOutOfRangeException>(() => Exercises.Grade(score));
+
+                }
+            }
+        }
+
+        public class MaxWeddingNumbersTests
+        {
+            [TestCase(4, 20)]
+            [TestCase(3, 50)]
+            [TestCase(2, 50)]
+            [TestCase(1, 100)]
+            [TestCase(0, 200)]
+            public void GivenCovidLevel_GetScottishMaxWeddingNumbers_RetrunsMaxAttendees(int covidLevel, int expected)
+            {
+                Assert.That(Exercises.GetScottishMaxWeddingNumbers(covidLevel), Is.EqualTo(expected));
+            }
+
+            [TestCase(5)]
+            [TestCase(-1)]
+            public void GivenCovidLevel_GetScottishMaxWeddingNumbers_RetrunsMaxAttendees(int covidLevel)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => Exercises.GetScottishMaxWeddingNumbers(covidLevel));
+            }
         }
     }
 }
