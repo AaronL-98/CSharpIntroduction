f828b35388db3072eb2e39eb6653b1cfd7ff4745
ALewis <ALewis@spartaglobal.com>
2023-04-19 15:28:10 +0100

    strings commit

--- MoreDataTypes_Lab/MoreDataTypes/Program.cs
+++ MoreDataTypes_Lab/MoreDataTypes/Program.cs
@@ -9,6 +9,9 @@ namespace MoreDataTypes
             var myString = "  C# list fundamentals  ";
             Console.WriteLine(StringExercise(myString));
             Console.WriteLine(StringBuilderExercise(myString));
+            StringInterpolation("Aaron");
+            ParsingStrings();
+
         }
 
         public static string StringExercise(string myString)
@@ -33,6 +36,43 @@ namespace MoreDataTypes
             sb.Replace('L', '*').Replace('T', '*').Remove(nPos + 1, sb.Length - nPos - 1);
             
             return sb.ToString();
+        }
+
+        public static void StringInterpolation(string str)
+        {
+            Console.WriteLine("My name is : " + str + " using +");
+            Console.WriteLine(string.Concat("My name is : ", str, " using interpolation"));
+            Console.WriteLine($"My name is : {str} using interpolation" );
+
+            int num1 = 2;
+            var num2 = 7;
+            var fString = $"{num1} to the power of {num2} is {Math.Pow(num1, num2)}";
+            Console.WriteLine(fString);
+            var fString2 = $"Log to the base {num2} of {num1} is {Math.Log(num2, num1)}";
+            Console.WriteLine(fString2);
+            var fString3 = $"That will be £{num2 / 3.0} please!";
+            Console.WriteLine(fString3);
+        }
+
+        public static void ParsingStrings()
+        {
+            Console.WriteLine("How many apples?");
+            string input = Console.ReadLine(); ;
+            //bool success = Int32.TryParse(input, out int numApples);
+
+            while (!Int32.TryParse(input, out int numApples))
+            {
+                Console.WriteLine("Value must be an integer");
+                input = Console.ReadLine();
+            }
+            //try 
+            //{
+            //    int numApples = Int32.Parse(input);
+            //}
+            //catch (FormatException e)
+            //{
+            //    Console.WriteLine("must be and integer");
+            //}
         }
 
     }
